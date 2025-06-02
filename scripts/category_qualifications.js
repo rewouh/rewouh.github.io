b912e4efcac66ceefb265d761ef4231b09a3ef6c
Pierre B <pbraudpersonal@gmail.com>
2025-06-02 18:08:04 +0200

    small changes

--- scripts/category_qualifications.js
+++ scripts/category_qualifications.js
@@ -22,10 +22,11 @@ categoriesContents['qualifications'] = [
     ['typewriting_text', 
         {
             'text': {
-                'fr': ["Baccalauréat - Spécialités maths, mention bien.", "", "TOEIC 965.", "", "Diplôme d'ingénieur de l'\u200BEPITA\u200B (obtention prévu en 2026)."].join('\n'),
-                'us': ["French Baccalauréat - Science stream (Maths major), with honors.", "", "TOEIC 965.", "", "Engineering degree from \u200BEPITA\u200B (expected in 2026)."].join('\n')
+                'fr': ["Baccalauréat - Spécialités maths, mention bien.", "", "TOEIC 965.", "", "Moyenne de première année de classes préparatoires : \u00AD16.65/20\u00AD (Moyenne de promo inconnue)", "Moyenne de seconde année de classes préparatoires : \u00AD16.8/20\u00AD (Moyenne de promo inconnue)", "Moyenne première année de cycle ingénieur : \u00AD17.35/20\u00AD (Moyenne de promo: \u00AD13.62/20\u00AD)", "", "Diplôme d'ingénieur de l'\u200BEPITA\u200B (obtention prévu en 2026)."].join('\n'),
+                'us': ["French Baccalauréat - Science stream (Maths major), with honors.", "", "TOEIC 965.", "", "Preparatory classes first year grade average : \u00AD16.65/20\u00AD (Class average unknown)", "Preparatory classes second year grade average : \u00AD16.8/20\u00AD (Class average unknown)", "First engineering year grade average : \u00AD17.35/20\u00AD (Class average: \u00AD13.62/20\u00AD)", "", "Engineering degree from \u200BEPITA\u200B (expected in 2026)."].join('\n')
             },
-            'hyperlinks': ['https://www.epita.fr/en/']
+            'hyperlinks': ['https://www.epita.fr/en/'],
+            'classes': ['bold', 'bold', 'bold', 'bold']
         } 
     ]
 ]
